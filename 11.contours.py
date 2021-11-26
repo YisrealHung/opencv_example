435d1b32ccbf438ad0fcb8f2ca93e1c01e8bdf17
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2021-11-26 10:07:19 +0800

    Update 11.contours.py

--- 11.contours.py
+++ 11.contours.py
@@ -9,20 +9,32 @@ canny = cv2.Canny(blur, 30, 150)
 
 cnts, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+count = 0
+for i in cnts:
+    area = cv2.contourArea(i)
+    if area > 10000:
+        cv2.drawContours(img, cnts, count, (255, 0, 255), 2)
+    count += 1
+
+
+'''
 #cv2.drawContours(img, cnts, -1, (255, 0, 255), 2)
 for i in cnts:
+    # 取得周長
     peri = cv2.arcLength(i, True)
+
+    # 取得多邊形的曲線
     approx = cv2.approxPolyDP(i, 0.01 * peri, True)
-    
+
+    # 取得面積
     area = cv2.contourArea(i)
-    
+
     if area > 10000:
         cv2.drawContours(img, [approx], -1, (255, 255, 0), 3)
     print(area)
-
+'''
 cv2.imshow("canny", canny)
 cv2.imshow("image", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
